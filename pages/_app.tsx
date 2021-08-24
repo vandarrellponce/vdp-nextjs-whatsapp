7c5a5a85a24bf7eb4d9e1098959a96fc03c3e065
Van Darrell Ponce <riversideriver@yahoo.com>
2021-08-24 16:18:13 +0800

    capturing and storing user details

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from '../firebase'
+import { auth, db } from '../firebase'
 import Login from './login'
 import Loading from '../components/Loading'
+import firebase from 'firebase'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth)
+
+  useEffect(() => {
+    db.collection('users').doc(user.uid).set(
+      {
+        email: user.email,
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        photoURL: user.photoURL,
+      },
+      { merge: true }
+    )
+  }, [user])
 
   if (loading) return <Loading />
   if (!user) return <Login />
